aa1407c22a4629028c519004b077b79d944010c7
Soham <sohamgulhane007@gmail.com>
2024-02-18 10:50:10 +0530

    sample commit

---
+++ Untitled-2.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a = 123;
+    cout<<a<<endl;
+
+    char b = 'v';
+    cout<<b<<endl;
+
+    bool bl = true;
+    cout<<bl<<endl;
+
+    float c = 1.3;
+    cout<<c<<endl;
+    int size = sizeof(a);
+    cout<<"Size of a is : "<<sizeof(a)<<endl;
+
+    //type casiting
+    int e = 'a';
+    cout<<a<<endl;
+
+    char ch = 98;
+    cout<< ch << endl;
+
+    int f = 2/5;
+    cout<<f<<endl;
+    
+    cout<<2.0/5<<endl;
+
+    
+}
